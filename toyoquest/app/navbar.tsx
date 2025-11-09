bc29bb3662c72feae311b7ce7aca307fae642b7c
vishyyyyyyyyy <217375979+vishyyyyyyyyy@users.noreply.github.com>
2025-11-09 02:23:46 -0600

    sticky navbar!!

--- toyoquest/app/navbar.tsx
+++ toyoquest/app/navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav>
+     <nav className="sticky top-0 z-50 bg-white">
       <div className="flex flex-row justify-between items-center border-b-6 p-4 font-bold text-black bg-white">
        TOYOQUEST
        <img src="https://www.vectorlogo.zone/logos/toyota/toyota-tile.svg" alt="Toyota Logo" className="w-10 h-10 ml-2" >
