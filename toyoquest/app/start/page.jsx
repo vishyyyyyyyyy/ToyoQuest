44477a11bfc5ded701f9d4832c92c5fc55fd340e
Ji Yoo <jiyoopark05@gmail.com>
2025-11-09 03:32:29 -0600

    start finances page

--- toyoquest/app/start/page.jsx
+++ toyoquest/app/start/page.jsx
@@ -1,12 +1,18 @@
 import Navbar from '../navbar';
+import Link from 'next/link';
 export default function Start() {
     return (
         <>
         <Navbar />
-        <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gradient-to-b from-white to-red-100">
-            <div className="text-center"></div>
-                <h1 className="text-5xl font-bold text-red-600">Welcome to ToyoQuest!</h1>
-                </div>
+        <div className="w-[1512px] h-[982px] relative bg-white overflow-hidden">
+<div className="w-44 h-[845.08px] left-[1148px] top-[26.99px] absolute origin-top-left rotate-[-28deg] bg-red-200 rounded-3xl" />
+<div className="w-44 h-[469.20px] left-[857px] top-[-56.01px] absolute origin-top-left rotate-[-28deg] bg-red-600 rounded-3xl" />
+<Link className="w-96 h-24 bg-red-600 rounded-[107px] ml-[536px] mt-[559px] absolute justify-start text-white text-3xl font-bold font-['Inter']" href = "./financials">Get Started</Link>
+<div className="left-[332px] top-[329px] absolute justify-start text-black text-5xl font-bold font-['Inter']">Let’s talk finances!</div>
+<div className="w-[826px] left-[332px] top-[408px] absolute justify-start text-black text-3xl font-normal font-['Inter']">Before we dive in, tell us a bit about your budget and financial profile so we can find the perfect fit for you.</div>
+<div className="w-44 h-[996.37px] left-[-200px] top-[278.99px] absolute origin-top-left rotate-[-28deg] bg-red-400 rounded-3xl" />
+<div className="w-44 h-[595.87px] left-[259.40px] top-[675.49px] absolute origin-top-left rotate-[-28deg] bg-red-300 rounded-3xl" />
+</div>
                 </>
     )
 }
